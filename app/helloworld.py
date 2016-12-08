b4f0d2ec0520c7582d2b65a3ff0764df2614eb9e
HyeokJun\혁준 <tgw4460@naver.com>
2016-12-09 01:11:58 +0900

    Revert "post.html,  google materializecss 효과 적용(http://materializecss.com/)\"
    This reverts commit dc0c1d649afbd981943b619531f24437d96320e4.

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
-import json
 from flask import Flask, redirect, url_for, request, render_template
-
 app = Flask(__name__)
-
-
 
 
 @app.route('/')
@@ -17,10 +12,7 @@ def about():
 
 @app.route('/post')
 def post():
-   customer = '{"name": "강진수", "id": 152352, "history": [{"date": "2015-03-11", "item": "iPhone"}, {"date": "2016-02-23", "item": "Monitor"}]}'
-   decode_test = json.loads(customer)
-   print(decode_test['name'])
-   return render_template('post.html',  json_string = decode_test['name'])
+   return render_template('post.html')
 
 @app.route('/contact')
 def contact():
