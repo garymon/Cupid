5c0fe0b19eb78f22af1f7e947d46f572a0d47420
SeoHyeokJun <tgw4460@naver.com>
2016-12-10 22:31:42 +0900

    post.html, server 수정. post.json 생성해서 정보 주고 받도록 구현

--- app/helloworld.py
+++ app/helloworld.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import json
+import json, uuid
 from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
@@ -11,16 +11,39 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+def jsonController(myData) :
+   print(uuid.uuid4())
+
+   fRead = open("post.json", 'r')
+   originJson= fRead.read()
+   fRead.close()
+
+   fWrite = open("post.json", 'w')
+
+   originDataArr = json.loads(originJson)
+
+   originDataArr.append(myData)
+
+   fWrite.write(json.dumps(originDataArr))
+   fWrite.close()
+
+
 @app.route('/about')
 def about():
    return render_template('about.html')
 
-@app.route('/post')
+@app.route('/posts', methods=['POST', 'GET'])
 def post():
-   customer = '{"name": "강진수", "id": 152352, "history": [{"date": "2015-03-11", "item": "iPhone"}, {"date": "2016-02-23", "item": "Monitor"}]}'
-   decode_test = json.loads(customer)
-   print(decode_test['name'])
-   return render_template('post.html',  json_string = decode_test['name'])
+
+   if request.method == "GET":
+      fRead = open("post.json", 'r')
+      outputJson = json.loads(fRead.read())
+      outputJson.reverse()
+      return render_template('post.html', posts = outputJson)
+   elif request.method == "POST":
+      req_json = request.get_json()
+      jsonController(req_json)
+      return json.dumps(req_json)
 
 @app.route('/contact')
 def contact():
