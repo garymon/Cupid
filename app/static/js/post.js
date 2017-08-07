d6e89b5005e3abc4f22a8faf2fc4d2b2a0861c3a
Jun Seo <tgw4460@naver.com>
2017-08-07 21:44:31 +0900

    Complete post category

--- app/static/js/post.js
+++ app/static/js/post.js
@@ -25,7 +25,47 @@ $(document).ready(function() {
     })
 
     $(".postDelete").click(function() {
-        console.log("delete");
-        console.log($("this").parent("div"))
+        var postData = {uuid : $(this).parents(".postUuid").data('uuid')};
+
+        $.ajax({
+            type : "DELETE",
+            url : "/post",
+            contentType: "application/json",
+            data : JSON.stringify(postData),
+            success : function(args) {
+                window.location.reload();
+                console.log("post success", args);
+            },
+            error:function(e) {
+                console.log("Post postText Error", e);
+            }
+        });
+    })
+
+    $(".postUpdate").click(function() {
+        $(this).parents(".panel").find(".postContent").hide()
+        $(this).parents(".panel").find(".updateTextBox").show()
+    })
+
+
+
+    $(".updateButton").click(function() {
+        var updateText = $(this).parents(".updateTextBox").find(".updateText").val();
+        console.log(updateText);
+        var updateData = {content : updateText, name : "JUN SEO", date : "", uuid : $(this).parents(".postUuid").data('uuid')};
+
+        $.ajax({
+            type : "PUT",
+            url : "/post",
+            contentType: "application/json",
+            data : JSON.stringify(updateData),
+            success : function(args) {
+                window.location.reload();
+                console.log("update success", args);
+            },
+            error:function(e) {
+                console.log("update updateText Error", e);
+            }
+        });
     })
 })
